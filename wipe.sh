48a9c37e2db05b221c9dd37ef7219bce0b53ed31
Jackson Oates <jackson.oates@gmail.com>
2014-08-30 22:40:49 -0700

    added shebang for better portablity and cross platform usability

--- wipe.sh
+++ wipe.sh
@@ -1,7 +1,9 @@
+#!/usr/bin/env bash
+
 git pull
 read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   shopt -s dotglob;
-  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "private.xml" --exclude "wipe.sh" --exclude "README.md" -av ./** ~
+  rsync --dry-run --exclude ".git/" --exclude ".DS_Store" --exclude "private.xml" --exclude "wipe.sh" --exclude "README.md" -av ./** ~
 fi
